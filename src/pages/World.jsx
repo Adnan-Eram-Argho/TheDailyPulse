1f57ba00affb986d7af32070a38bfe02e4d1f27c
Adnan-Eram-Argho <adnaneramargho@gmail.com>
2024-06-04 18:18:07 +0600

    description fixed

--- src/pages/World.jsx
+++ src/pages/World.jsx
@@ -18,7 +18,7 @@ function World() {
       }
   return (
     <div>
-    <h1 className="text-center mt-5 text-4xl">Here are all the Tech News</h1>
+    <h1 className="text-center mt-5 text-4xl">Here are all the World News</h1>
 
     <div className="flex flex-wrap mt-5 mb-5 items-center justify-between">
       {news?.filter(worldfilter)?.map((singleNews) => (
